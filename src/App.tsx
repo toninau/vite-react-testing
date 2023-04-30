91c1752c310066d951b9e6cf4f1b8111b8d41d34
Toni Naumanen <toni.naumis@gmail.com>
2023-04-30 11:03:30 +0300

    Add react error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
-import Notes from './features/notes';
-
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { queryClient } from './lib/react-query';
+import { ErrorBoundary } from 'react-error-boundary';
+
+import Notes from '@/features/notes';
+import { queryClient } from '@/lib/react-query';
+
+function ErrorFallback({ error }: { error: Error }) {
+  return (
+    <div role="alert">
+      <h1>someting went wrong:</h1>
+      <pre style={{ color: 'red' }}>{error.message}</pre>
+      <button onClick={() => window.location.assign(window.location.origin)}>
+        refresh
+      </button>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Notes />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <QueryClientProvider client={queryClient}>
+        <Notes />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </ErrorBoundary>
   );
 }
 
